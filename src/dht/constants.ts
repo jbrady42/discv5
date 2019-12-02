bffa8ba5d5fc478c4f1e2ab7482214e4a9275b8c
Jason <jabrady42@gmail.com>
2019-12-02 16:04:38 -0600

    wip wip

---
+++ src/dht/constants.ts
@@ -0,0 +1,8 @@
+// Kademlia bucket size
+export const KAD_BUCKET_SIZE = 16;
+
+// 32 bytes
+export const HASH_BITS = 32 * 8;
+
+// Kademlia bucket size
+export const KAD_NUM_BUCKETS = HASH_BITS + 1;
