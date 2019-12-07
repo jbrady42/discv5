ba747ffc826c60b02c22b008974d4b1999f7e07a
Jason Brady <jabrady42@gmail.com>
2019-12-06 18:34:12 -0800

    Basic ping-pong

--- src/dht/types.ts
+++ src/dht/types.ts
@@ -1,41 +1,64 @@
 import {
   NodeId,
 } from "../enr";
+import { EventEmitter } from "events";
 
 type Topic = string;
 
 export interface INode {
-  id: NodeID;
+  id: NodeId;
 }
 
 export interface IServiceAd {
   topic: Topic;
 }
 
-export interface ILookupService {
-  registerTopic(Topic): Promise<bool>;
-  searchTopic(Topic): Promise<INode[]>;
-  lookup(NodeID): Promise<INode[]>;
-  updateLocalState(INode): Promise<Null>;
+export enum MessageType {
+  PING = 1,
+  PONG = 2,
+  FINDNODE = 3,
+  NODES = 4,
+  REGTOPIC = 5,
+  TICKET = 6,
+  REGCONFIRM = 7,
+  TOPICQUERY = 8,
 }
+
+export interface IMessage {
+  type: MessageType;
+  requestId: number;
+  data: Buffer;
+}
+
+export interface IDiscv5 {
+  registerTopic(t: Topic): Promise<boolean>;
+  searchTopic(t: Topic): Promise<INode[]>;
+  lookup(id: NodeId): Promise<INode[]>;
+  updateLocalState(node: INode): Promise<null>;
+}
+
+export interface INetworkService extends EventEmitter {
+  sendMessages(msgs: IMessage[]): Promise<void>;
+}
+
 
 export interface IKadTable {
   readRandomNodes(): Promise<INode[]>;
-  getRefreshTarget(): Promise<NodeID>;
-  closest(NodeID, Number): Promise<INode[]>;
-  add(INode): Promise<Null>;
+  getRefreshTarget(): Promise<NodeId>;
+  closest(id: NodeId, num: number): Promise<INode[]>;
+  add(node: INode): Promise<void>;
 }
-
-export interface IAdService {
-  handleAdPublish(IServiceAd)
-}
-
-
-export interface ITicketService {
-  newTicket(): ITicket
-  maybeAcceptTicket(ITicket): bool
-}
-
-export interface IAdTable {
-  nextAvailableTime(Topic)
-}
+//
+// export interface IAdService {
+//   handleAdPublish(IServiceAd)
+// }
+//
+//
+// export interface ITicketService {
+//   newTicket(): ITicket
+//   maybeAcceptTicket(ITicket): bool
+// }
+//
+// export interface IAdTable {
+//   nextAvailableTime(Topic)
+// }
